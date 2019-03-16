c88da3dbcb715269b7964d9334cd5648e85ba3b1
Jonathan <han_082781@yahoo.com>
2019-03-16 20:47:43 +0800

    Create delete capability

--- src/main/java/com/ppmt/services/ProjectService.java
+++ src/main/java/com/ppmt/services/ProjectService.java
@@ -37,4 +37,14 @@ public class ProjectService {
 	public Iterable<Project> findAllProjects(){
 		return projectRepository.findAll();
 	}
+	
+	public void deleteProjectByIdentifier(String projectId) {
+		Project project = projectRepository.findByProjectIdentifier(projectId);
+		
+		if(project == null) {
+			throw new ProjectIdException("Can't delete Project with ID '"+ projectId +"'. This project doesn't exist.");
+		}
+		
+		projectRepository.delete(project);
+	}
 }
